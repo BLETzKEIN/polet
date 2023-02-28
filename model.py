856ed606587cee79287844f4aaf1b9ccd76d64f1
wert <fhvfhvb@gmail.com>
2023-02-28 12:05:26 +0500

    n1

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
 import pygame
 rect=pygame.Rect(100,100,200,300)
+rect_money=pygame.Rect(0,0,50,50)
 speedy=1
 speedx=1
 WIDTH=1400
 HEIGHT=700
+display=pygame.display.get_surface()
 
 def update():
     global speedy
@@ -20,3 +22,14 @@ def update():
 
     rect.centerx+=speedx
     rect.centery+=speedy
+def display_full():
+    global WIDTH,HEIGHT
+    if pygame.display.is_fullscreen():
+        WIDTH = 1400
+        HEIGHT = 700
+        pygame.display.set_mode([WIDTH, HEIGHT])
+    else:
+        pygame.display.set_mode([0, 0], pygame.FULLSCREEN)
+        WIDTH = display.get_width()
+        HEIGHT = display.get_height()
+
