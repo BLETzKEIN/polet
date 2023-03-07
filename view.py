6a8fef01a0a4e081c3d0e29d2c50f717c16094e2
wert <fhvfhvb@gmail.com>
2023-03-07 11:12:07 +0500

    n3

--- view.py
+++ view.py
@@ -11,9 +11,9 @@ baller=pygame.transform.scale(balle,[250,model.HEIGHT])
 
 def weiv():
     display.fill([0, 0, 0])
-    pygame.draw.rect(display, [55, 98, 205], [model.WIDTH - 300, 0, 250, model.HEIGHT])
+    # pygame.draw.rect(display, [55, 98, 205], [model.WIDTH - 300, 0, 250, model.HEIGHT])
     #pygame.draw.rect(display, [90, 179, 202], model.rect_money)
-    pygame.draw.rect(display, [78, 190, 59], model.rect)
+    # pygame.draw.rect(display, [78, 190, 59], model.rect)
     display.blit(baller,[model.WIDTH-300,0])
     display.blit(bitcoin,model.rect_money)
     display.blit(elon_mask,model.rect)
